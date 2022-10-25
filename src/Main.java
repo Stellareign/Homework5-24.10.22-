ba47a169f7a7159caa2dba7cc2974270accaceae
Stellareign <tuguran@mail.ru>
2022-10-25 21:04:07 +0300

    Задание 1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Р”РѕРјР°С€РєР° 5, Р·Р°РґР°РЅРёРµ 1:");
+        int age = 16;
+        if (age >= 18) {
+            System.out.println("РџРѕР·РґСЂР°РІР»СЏСЋ СЃ СЃРѕРІРµСЂС€РµРЅРЅРѕР»РµС‚РёРµРј!");
+        }
+        if (age < 18) {
+            System.out.println("РЎРѕРІРµСЂС€РµРЅРЅРѕР»РµС‚РёРµ РµС‰С‘ РЅРµ РЅР°СЃС‚СѓРїРёР»Рѕ, РїСЂРґС‘С‚СЃСЏ РїРѕРґРѕР¶Р°С‚СЊ.");
+        }
+    }
+}
